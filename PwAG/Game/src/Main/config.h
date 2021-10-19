fb17a512c407d9893576d4c015e262f3682be925
Pawel Salicki <pawel.salicki@gmail.com>
2021-10-19 20:18:18 +0200

    Resolution change

--- PwAG/Game/src/Main/config.h
+++ PwAG/Game/src/Main/config.h
@@ -5,8 +5,8 @@ namespace Config
 {
 	constexpr WindowMode g_defaultWindowMode = WindowMode::eWindowed;
 
-	constexpr uint32_t g_defaultWidth = 800;
-	constexpr uint32_t g_defaultHeight = 600;
+	constexpr uint32_t g_defaultWidth = 1600;
+	constexpr uint32_t g_defaultHeight = 900;
 
 	constexpr const char* g_defaultWindowTitle = "PwAG Project!";
 }
