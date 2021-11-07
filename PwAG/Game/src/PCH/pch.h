363265c28f57f54cece51b660bf5ba1501559e4d
Pawel Salicki <pawel.salicki@gmail.com>
2021-11-07 16:51:34 +0100

    odkomentowanie stb_image w pch.h

--- PwAG/Game/src/PCH/pch.h
+++ PwAG/Game/src/PCH/pch.h
@@ -26,12 +26,8 @@
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
-// podobno (tak ludzie ze stacka pisz¹) to musi byæ definiowane w pliku .cpp lub .c, bo nie da siê skompilowaæ przy próbie u¿ycia funkcji stbi_load lub free
-// da³em to na razie w texture.cpp
-/*
 #define STB_IMAGE_IMPLEMENTATION
 #include "../SourceDep/stb_image.h"
-*/
 
 // this project
 #include "../Main/config.h"
